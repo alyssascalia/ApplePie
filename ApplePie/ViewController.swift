bf4b0207a0d5fa1cc6da78a7daa2791cb6cbdc4b
Alyssa Scalia <a.scalia@setonhill.edu>
2023-04-07 21:20:18 -0400

    ready

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var listOfWords = ["gorilla", "pilgrimage", "can", "orphanage", "mafia", "spaghetti"]
+    
+    let incorrectMovesAllowed = 6
+    
+    var totalWins = 0
+    var totalLosses = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    var currentGame: Game!
+    
+    func newRound() {let newWord = listOfWords.removeFirst()
+    currentGame = Game(word: newWord, incorrectMovesRemaining:
+        incorrectMovesAllowed, guessedLetters: [])
+        updateUI()
+    }
+    
+    func updateUI() {
+        correctWordLabel.text = currentGame.formattedWord
+        scoreLabel.text = "Wins: \(totalWins) Losses:  \(totalLosses)"
+        treeImage.image = UIImage (named: "Tree  (currentGame.incorrectMovesRemaining)")
+    }
+    
+    @IBOutlet var treeImage: UIImageView!
+    
+    @IBOutlet var correctWordLabel: UILabel!
+    
+    @IBOutlet var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) { sender.isEnabled = false
+        let letterString = sender.configuration!.title!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        func updateGameSate() {
+            if
+        }
+    }
+    
+    
 }
-
